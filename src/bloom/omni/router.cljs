c1cd1dbe27dcadb52fcb3effcd19ce29354eef49
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-03-01 05:14:32 +0800

    Change router interface to only expose fx and defroute

--- src/bloom/omni/router.cljs
+++ src/bloom/omni/router.cljs
@@ -1,25 +1,39 @@
 (ns bloom.omni.router
-  "Provides cljs functions for client-side routing system:
-  
-  (router/init!) to initiate the router
+  "Wrapper for secretary + accoutnant + re-frame based client-side routing.
 
-  (router/defroute ...) to defining secretary routes
+  Provides defroute macro for registering routes and an fx for re-frame.
+
+  (router/defroute ...) to define secretary routes
+
+  (reg-fx :router router/fx) to register the fx
   
-  (reg-fx :navigate router/navigate-fx) to define a re-fram fx"
+  {:router [:init!]} to initiate the router (should only be called once)
+
+  {:router [:dispatch-current!]} to trigger route for the current URL
+
+  {:router [:navigate! \"path\"]} to navigate browser to the provided path (and dispatch)"
   (:require
     [accountant.core :as accountant]
     [secretary.core :as secretary])
   (:require-macros
     [bloom.omni.router :refer [defroute]]))
 
-(defn init! []
-  (accountant/configure-navigation! {:nav-handler secretary/dispatch!
-                                     :path-exists? secretary/locate-route})
+(defn- navigate! [path]
+  (accountant/navigate! path))
+
+(defn- dispatch-current! []
   (accountant/dispatch-current!))
 
-(defn navigate-fx 
+(defn- init! []
+  (accountant/configure-navigation! {:nav-handler secretary/dispatch!
+                                     :path-exists? secretary/locate-route})
+  (dispatch-current!))
+
+(defn fx 
   "Fx for use with re-frame"
-  [path]
-  (if (= path :current-path)
-    (accountant/dispatch-current!)
-    (accountant/navigate! path)))
+  [[command & args]]
+  (case command
+    :init! (init!)
+    :dispatch-current! (dispatch-current!)
+    :navigate! (apply navigate! args)))
+
