7fcfd75a4a68748cc2a32b3a43b2eb541dc1c294
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-04-04 19:53:58 +0800

    Change CSS watcher to only touch files if the result has changed

--- src/bloom/omni/css_watcher.clj
+++ src/bloom/omni/css_watcher.clj
@@ -15,22 +15,41 @@
     [bloom.omni.impl.css :as css]
     [bloom.omni.impl.async :as async]))
 
+(def previous-output-hash (atom nil))
+
+(def compile! 
+  ; debounce, b/c some editors trigger multiple events on a file
+  ; causing figwheel to send multiple css files, which is janky
+  ; just filtering for :modify events isn't sufficient
+  (async/debounce 
+    (fn [{:keys [output-to styles] :as css-config}]
+      (try
+        (do
+          (.mkdir (java.io.File. (.getParent (java.io.File. output-to))))
+
+          (require (symbol (namespace styles)) :reload)
+
+          (let [output (css/compile-css css-config)
+                output-hash (hash output)]
+            (when (not= output-hash @previous-output-hash)
+              (spit output-to output)
+              (reset! previous-output-hash output-hash))))
+        (catch Exception e
+          (println "Unable to compile CSS due to error.")
+          (println e))))
+    50))
+
 (defn- stop-watcher! [watcher]
   (println "Stopping CSS watcher...")
   (hawk/stop! watcher))
 
 (defn- start-watcher! [css-config]
   (println "Starting CSS watcher...")
+  (reset! previous-output-hash nil)
   (let [css-config (merge css-config
-                          {:pretty-print? true})
-        ; debounce, b/c some editors trigger multiple events on a file
-        ; causing figwheel to send multiple css files, which is janky
-        ; just filtering for :modify events isn't sufficient
-        compile! (async/debounce 
-                   #(css/compile! css-config)
-                   50)]
+                          {:pretty-print? true})]
 
-    (compile!)
+    (compile! css-config)
      
     (hawk/watch! [{:paths ["src"]
                    :handler
@@ -38,7 +57,7 @@
                      (when (and 
                              (.isFile file)
                              (string/ends-with? (.getName file) "cljc"))
-                       (compile!))
+                       (compile! css-config))
                      nil)}])))
 
 (mount/defstate component
