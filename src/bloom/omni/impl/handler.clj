5afc230857ba5c6c60a9a7d8dc2cdfb0baff95d8
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-04-18 20:09:53 +0800

    Remove stale require

--- src/bloom/omni/impl/handler.clj
+++ src/bloom/omni/impl/handler.clj
@@ -2,8 +2,7 @@
   (:require
     [mount.core :as mount]
     [bloom.omni.ring :as ring]
-    [bloom.omni.impl.config :refer [config]]
-    [ring.middleware.format :refer [wrap-restful-format]]))
+    [bloom.omni.impl.config :refer [config]]))
 
 (mount/defstate handler
   :start (apply ring/combine (config :handlers))
