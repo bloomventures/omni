bed0a93a8b2c969703e24ea28e604b0f56f516b4
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-05-10 16:27:50 +0300

    Generate static gzip files for compiled js and css

--- src/bloom/omni/compile.clj
+++ src/bloom/omni/compile.clj
@@ -1,15 +1,22 @@
 (ns bloom.omni.compile
   (:require
-    [bloom.omni.impl.cljsbuild :as cljsbuild]
-    [bloom.omni.impl.cssbuild :as cssbuild]
     [bloom.omni.impl.builds :refer [builds]]
-    [bloom.omni.impl.config :as config]))
+    [bloom.omni.impl.gzip :as gzip]
+    [bloom.omni.impl.cljsbuild :as cljsbuild]
+    [bloom.omni.impl.config :as config]
+    [bloom.omni.impl.cssbuild :as cssbuild]))
 
 (defn compile-css! [c]
-  (cssbuild/compile! (:css (config/fill c))))
+  (cssbuild/compile! (:css (config/fill c)))
+  (gzip/compress (get-in (config/fill c) [:css :output-to])))
     
 (defn compile-js! [c]
-  (cljsbuild/compile! (last (builds (config/fill c)))))
+  (let [prod-build (->> (builds (config/fill c))
+                        (filter (fn [b]
+                                  (= "prod" (b :id))))
+                        first)]
+    (cljsbuild/compile! prod-build)
+    (gzip/compress (get-in prod-build [:compiler :output-to]))))
 
 (defn compile! [c]
   (compile-css! c)
