086fce5a2e10b5b0c2d61f49dd25d487b5d96b97
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-05-10 21:00:06 +0300

    Touch compiled files to have the same time (nginx prefers it that way)

--- src/bloom/omni/compile.clj
+++ src/bloom/omni/compile.clj
@@ -1,22 +1,32 @@
 (ns bloom.omni.compile
   (:require
+    [clojure.java.io :as io]
     [bloom.omni.impl.builds :refer [builds]]
     [bloom.omni.impl.gzip :as gzip]
     [bloom.omni.impl.cljsbuild :as cljsbuild]
     [bloom.omni.impl.config :as config]
     [bloom.omni.impl.cssbuild :as cssbuild]))
 
+(defn touch [& paths]
+  (let [t (System/currentTimeMillis)]
+    (doseq [path paths]
+      (.setLastModified (io/file path) t))))
+
 (defn compile-css! [c]
-  (cssbuild/compile! (:css (config/fill c)))
-  (gzip/compress (get-in (config/fill c) [:css :output-to])))
+  (let [path (get-in (config/fill c) [:css :output-to])]
+    (cssbuild/compile! (:css (config/fill c)))
+    (gzip/compress path)
+    (touch path (str path ".gz"))))
     
 (defn compile-js! [c]
   (let [prod-build (->> (builds (config/fill c))
                         (filter (fn [b]
                                   (= "prod" (b :id))))
-                        first)]
+                        first)
+        path (get-in prod-build [:compiler :output-to])]
     (cljsbuild/compile! prod-build)
-    (gzip/compress (get-in prod-build [:compiler :output-to]))))
+    (gzip/compress path)
+    (touch path (str path ".gz"))))
 
 (defn compile! [c]
   (compile-css! c)
