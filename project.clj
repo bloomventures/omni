b095f9d0c987feeb0daf8c7814ad2a340fb5d00f
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-05-15 16:19:42 +0300

    Release 0.14.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.bloomventures/omni "0.14.1"
+(defproject io.bloomventures/omni "0.14.2"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
 
