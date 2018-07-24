e2906733b3246c2a355c23f2b6e8922bfa6eea03
James N. V. Cash <james.nvc@gmail.com>
2018-07-24 12:30:17 -0400

    Release 0.14.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.bloomventures/omni "0.14.9-SNAPSHOT"
+(defproject io.bloomventures/omni "0.14.9"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
 
