2c5ac77d4a05eaa64b239f5fffecb1d2ef338838
James N. V. Cash <james.nvc@gmail.com>
2018-11-22 09:28:09 -0500

    Start 0.17.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.bloomventures/omni "0.17.5"
+(defproject io.bloomventures/omni "0.17.6-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
 
