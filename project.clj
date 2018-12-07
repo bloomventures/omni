264b7ce4d8ee601fbc21a4fd9fd8d56b4a7784df
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-12-07 14:33:18 -0500

    Start 0.17.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.bloomventures/omni "0.17.9"
+(defproject io.bloomventures/omni "0.17.10-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
 
