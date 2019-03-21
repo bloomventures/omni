28481f22a810e76e2ad7c7b73d2df6faec90309d
Rafal Dittwald <rafal.dittwald@gmail.com>
2019-03-21 19:05:10 -0400

    Release 0.20.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.bloomventures/omni "0.20.1-SNAPSHOT"
+(defproject io.bloomventures/omni "0.20.1"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
 
