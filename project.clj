cb79a632dcc9eeb915374bda266c05b43e593ad2
James N. V. Cash <james.nvc@gmail.com>
2019-04-08 16:04:21 -0400

    Make version snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.bloomventures/omni "0.20.3"
+(defproject io.bloomventures/omni "0.20.3-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
 
