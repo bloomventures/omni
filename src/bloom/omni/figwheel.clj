4f04e29c55c3719901153fcea82c66c98f052e74
Rafal Dittwald <rafal.dittwald@gmail.com>
2025-03-24 13:14:05 -0400

    Fix error when no css directory

--- src/bloom/omni/figwheel.clj
+++ src/bloom/omni/figwheel.clj
@@ -7,16 +7,20 @@
                     :cljs {:main \"app.core\"}})
   ```"
   (:require
+    [clojure.java.io :as io]
     [figwheel.main.api :as repl-api]
     [bloom.omni.impl.builds :refer [builds]]))
 
 (defn start! [{:keys [cljs-opts server-port css?]}]
   (let [[build] (filter #(= "dev" (:id %)) (builds (assoc cljs-opts :css? css?)))]
+    ;; figwheel watch throws if directory does not exist
+    (when-let [css-dir (-> build :figwheel :css-dirs first)]
+      (.mkdirs (io/file css-dir)))
     (repl-api/start
-      {:id (:id build)
-       :options (:compiler build)
-       :config (assoc (:figwheel build)
-                      :ring-server-options {:port server-port})})))
+     {:id (:id build)
+      :options (:compiler build)
+      :config (assoc (:figwheel build)
+                :ring-server-options {:port server-port})})))
 
 (defn stop! []
   (repl-api/stop-all))
